1f58383e7fc3fc63cc2f35cf748fb0aa5e04937b
J.Marx <jennyrutledge@gmail.com>
2015-01-12 12:58:34 -0500

    Assessment complete

--- lib/assessment.rb
+++ lib/assessment.rb
@@ -6,7 +6,22 @@
 # string_it_x_times(4) #=> "4444"
 # string_it_x_times(2) #=> "22"
 def string_it_x_times(x)
+  #numInt = x
+  #numStr = numInt.to_s
+  numStr = x.to_s
+  #puts x.class
+  #puts numInt.class
+  #returns (numInt*numStr)
+
+  #arr = []
+  #numInt.times {arr << numStr}
+  #x.times {arr << numStr}
+  return (numStr*x)
+  #return arr
 end
+
+#puts "Please enter a number"
+#puts "#{string_it_x_times(gets.chomp)}"
 
 # Question 2
 # This function should take an integer as an argument
@@ -21,7 +36,22 @@ end
 # fizz_or_buzz(15) #=> "fizzbuzz"
 # fizz_or_buzz(2) #=> 2
 def fizz_or_buzz(x)
+
+  if x % 3 == 0 && x % 5 == 0
+    return "fizzbuzz"
+  elsif x % 5 == 0
+    return "buzz"
+  elsif x % 3 == 0
+    return "fizz"
+  else
+    return x
+  end
 end
+
+#puts fizz_or_buzz(3)
+#puts fizz_or_buzz(15)
+#puts fizz_or_buzz(100)
+#puts fizz_or_buzz(19)
 
 # Question 3
 # This function should take an array and an element of any type
@@ -31,7 +61,11 @@ end
 # add_element_to_array([1, 2], 3) #=> [1, 2, 3]
 # add_element_to_array(['fizz', 'buzz'], 'baz') #=> ['fizz', 'buzz', 'baz']
 def add_element_to_array(array, item)
+  outArr = array << item
+  return outArr
 end
+
+#puts "#{add_element_to_array([5,4,3,2], 1)}"
 
 
 
@@ -49,4 +83,26 @@ end
 # blackjack?(10, 'a') #=> true
 # blackjack?(5, 4) #=> false
 def blackjack?(card_one, card_two)
+  #Hash to define card
+  face =
+    {
+      'j'=>11
+    }
+  cardSum = 0
+  #puts "#{card_one.class == Fixnum}"
+  if card_one.class == Fixnum && card_two.class == Fixnum
+    cardSum = card_one + card_two
+    returns cardSum
+  elsif card_one.class == Fixnum
+
+
+  elsif card_two.class == Fixnum
+
+  else
+
+
+  end
+
 end
+
+#puts "#{blackjack?(1,4)}"
